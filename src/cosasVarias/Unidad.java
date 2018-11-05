5a5873c76c9757b02ddc2c884cbc359554f46fb0
Alvaro Iribarren <alvaro_iribarren@hotmail.com>
2018-11-05 12:58:03 -0300

    Segundo commit

---
+++ src/cosasVarias/Unidad.java
@@ -0,0 +1,16 @@
+package cosasVarias;
+
+public abstract class Unidad {
+	
+	private int vida;
+	
+	public Unidad(int vidaRecibida) {
+		this.vida= vidaRecibida;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hasta acá llegue correctamente");
+	}
+
+}
